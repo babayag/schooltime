9563855bf6ee878d59c84b46053ec8fbca3e3326
babayag <ironclock6@gmail.com>
2019-04-22 18:43:44 +0200

    initial commit and setup Environment

---
+++ src/main/java/com/mastermin/schooltime/SchooltimeApplication.java
@@ -0,0 +1,13 @@
+package com.mastermin.schooltime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchooltimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchooltimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mastermin/schooltime/SchooltimeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mastermin.schooltime;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SchooltimeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
